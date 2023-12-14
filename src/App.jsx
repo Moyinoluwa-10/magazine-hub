5f754f10fd80c39453f43ba6980dcc54a851b1d6
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-12-14 06:09:48 -0600

    feat: added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path={"/"} element={<Home />} />
+      <Route path={"/register"} element={<Register />} />
+      <Route path={"/login"} element={<Login />} />
+      <Route path={"/profile"} element={<Profile />} />
+      <Route path={"/dashboard"} element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;

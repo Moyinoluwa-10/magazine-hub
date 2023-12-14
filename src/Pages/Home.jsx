174ff842fac5522b126260a11ffe09387fc0f9cc
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-12-14 07:11:34 -0600

    feat: sidebar done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,13 @@
+import Catalog from "../Components/Catalog";
+import SideBar from "../Components/SideBar";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="grid h-screen min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]">
+      <SideBar />
+      <Catalog />
+    </div>
+  );
 };
 
 export default Home;

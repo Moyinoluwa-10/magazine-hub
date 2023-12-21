0d006166918322d5cefadd8cad115e841a626486
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-12-21 17:26:30 -0600

    feat:header component edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,16 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaAngleDown } from "react-icons/fa6";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen((open) => !open);
+  };
   return (
-    <header className="grid h-[60px] w-full overflow-hidden lg:grid-cols-[280px_1fr] border-b bg-gray-100/40 shadow-md">
-      <div className="flex items-center px-6 border-r">
+    <header className="grid h-[60px] w-full overflow-hidden sm:grid-cols-[280px_1fr] border-b bg-gray-100/40 shadow-md">
+      <div className="hidden sm:flex items-center px-6 border-r">
         <Link className="flex items-center gap-2 font-semibold" href="#">
           <IoNewspaper />
           <span className="">MagazineHub</span>
@@ -17,9 +22,30 @@ const Header = () => {
           <span className="sr-only">Toggle notifications</span>
         </button>
       </div>
-      <div className="flex items-center gap-4 px-6 shadow-2xl">
-        <div className="flex gap-2 items-center text-base cursor-pointer ml-auto">
-          <FaRegUserCircle /> My Account <FaAngleDown />
+
+      <div className="flex items-center gap-4 px-6">
+        <div
+          className="flex gap-2 items-center text-base cursor-pointer ml-auto"
+          onClick={handleToggle}
+        >
+          <FaRegUserCircle /> My Account{" "}
+          <FaAngleDown
+            className={
+              isOpen
+                ? "rotate-180 transition-all duration-200"
+                : "transition-all duration-200"
+            }
+          />
+        </div>
+        <div
+          className={
+            isOpen
+              ? "fixed right-6 top-12 p-5 bg-white border border-gray-400 rounded-md w-40 z-10 transition-all flex flex-col gap-1"
+              : "fixed right-6 top-12 p-5 bg-white border border-gray-400 rounded-md w-40 z-10 transition-all hidden"
+          }
+        >
+          <Link to={"/login"}>Log in</Link>
+          <Link to={"/signup"}>Sign-up</Link>
         </div>
       </div>
     </header>

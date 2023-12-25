443c46d7d12824b7e87458007cd6715674d32b0e
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-12-25 01:47:48 -0600

    chore: responsiveness adjustment

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -8,7 +8,7 @@ const Checkout = () => {
     <>
       <Header />
       <div
-        className="grid md:grid-cols-[280px_1fr] w-full overflow-hidden"
+        className="grid md:grid-cols-[280px_1fr] w-full overflow-hidden mt-16 md:mt-0"
         style={{ height: "calc(100vh - 60px)" }}
       >
         <SideBar />
